45f214c258588c099609ea201e8ec3ce190f3cdb
gocha001 <kalianichgocha@ukr.net>
2025-03-19 23:21:20 +0200

    add initial files svg img

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "modern-normalize";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
+// import { PersistGate } from "redux-persist/integration/react";
+// import Loader from "./components/Loader/Loader.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
+  </StrictMode>
+);
